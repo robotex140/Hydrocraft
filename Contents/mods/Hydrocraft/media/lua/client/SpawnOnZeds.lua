fbe0d506a71ca8576a402467dac93c5b31ae3a6c
HugoQwerty <HugoQwertyZomboid@protonmail.com>
2022-03-25 11:01:53 +0000

    Sandbox option
    Added an option to the Sandbox settings for whether or not HC guns should spawn on zeds.  Also tweaked the spawning code to not increase spawn chances of other items if guns are disabled.

--- Contents/mods/Hydrocraft/media/lua/client/SpawnOnZeds.lua
+++ Contents/mods/Hydrocraft/media/lua/client/SpawnOnZeds.lua
@@ -1,23 +1,67 @@
 
-local ROLLS = { 1, 1, 2, 2, 2, 2, 2, 2, 3, 3 } --number of rolls will be picked from this list
+--Each time a zed dies, a random number of items will be placed on the corpse
+--the number of items will be picked at random from this list
+local ROLLS = { 1, 1, 2, 2, 2, 2, 2, 2, 3, 3 } 
 
---easy to turn off if so desired.
---Maybe we could add a settings menu at some point.
-local SPAWN_MONEY = true 
-local SPAWN_HC_GUNS = true
-
+local NOTHING = "$nothing$"
+local TOTAL = 0
 local LOOT = {}
--- Money --
-	if(SPAWN_MONEY) then
+LOOT[NOTHING] = 0
+
+local function pickRandomItem()
+	local r = ZombRandFloat(0.0, TOTAL)
+	local tmp = 0
+	for k, v in pairs(LOOT) do
+		tmp = tmp + LOOT[k]
+		if(r < tmp) then
+			if(k == NOTHING) then
+				return nil
+			end
+			return InventoryItemFactory.CreateItem(k)
+		end
+	end	
+	return nil--should never happen, but if it does nothing bad happens.
+end
+
+local function SpawnLootOnDeadZombie(zombie)
+	
+	local randomRolls = ROLLS[ZombRand(#(ROLLS)) + 1]
+	--print("randomRolls = ", randomRolls)
+	
+	for i=1, randomRolls, 1 do
+		local item = pickRandomItem()--can be nil if dice roll is NOTHING
+		if(item ~= nil) then
+			zombie:getInventory():addItem(item)
+		end
+	end
+	
+end
+Events.OnZombieDead.Add(SpawnLootOnDeadZombie)
+
+local function OnLoadSpawnOnZeds()
+
+	local SPAWN_MONEY = true --no sandbox setting for money, yet.
+	local SPAWN_HC_GUNS = true
+	
+	if(SandboxVars.Hydrocraft.SpawnHydrocraftGuns ~= nil) then
+		SPAWN_HC_GUNS = SandboxVars.Hydrocraft.SpawnHydrocraftGuns
+	end
+	
+	--print("SPAWN_HC_GUNS = ", SPAWN_HC_GUNS)
+	
+	-- Money --
+	if(SPAWN_MONEY) then --if you edit these values, also edit the value in the else clause
 		LOOT["Hydrocraft.HCPenny"] = 1
 		LOOT["Hydrocraft.HCNickel"] = 1
 		LOOT["Hydrocraft.HCDime"] = 1
 		LOOT["Hydrocraft.HCQuarter"] = 1
 		LOOT["Hydrocraft.HCHalfdollar"] = 1
 		LOOT["Hydrocraft.HCDollar"] = 1
+	else
+		LOOT[NOTHING] = LOOT[NOTHING] + 6 --prevents disabling money from boosting spawn rates.
 	end
 
--- Weapons/Tools --
+	-- Weapons/Tools --
 	LOOT["Base.Lighter"] = 1
 	LOOT["Base.Matches"] = 1
 	LOOT["Hydrocraft.HCSurvivalaxe"] = 0.06
@@ -31,22 +75,25 @@ local LOOT = {}
 	LOOT["Hydrocraft.HCCompass"] = 0.1
 	LOOT["Hydrocraft.HCCamera"] = 0.05
 	LOOT["Base.Torch"] = 0.1
-	LOOT["Hydrocraft.HCFlashlightoff"] = 0.5
+	LOOT["Hydrocraft.HCFlashlightoff"] = 0.25
+	LOOT["Hydrocraft.HCFlashlighton"] = 0.25
 	LOOT["Hydrocraft.HCManometer"] = 0.04
 	LOOT["Hydrocraft.HCTrashsmall"] = 0.2
 	LOOT["Hydrocraft.HCFile"] = 0.05
 	LOOT["Hydrocraft.HCCalculator"] = 0.1
 
-	if(SPAWN_HC_GUNS) then
+	if(SPAWN_HC_GUNS) then --if you edit these values, also edit the value in the else clause
 		LOOT["Hydrocraft.HCMagAA12"] = 0.05
 		LOOT["Hydrocraft.HCAA12"] = 0.06
 		LOOT["Hydrocraft.HCUziSilencer"] = 0.05
 		LOOT["Hydrocraft.HCUzi"] = 0.06
 		LOOT["Hydrocraft.HCMagUZI"] = 0.05
 		LOOT["Hydrocraft.HCShotgunSilencer"] = 0.05
+	else
+		LOOT[NOTHING] = LOOT[NOTHING] + 0.32 --prevents disabling guns from boosting spawn rates.
 	end
 
--- Clothes --
+	-- Clothes --
 	LOOT["Hydrocraft.HCWorkgloves"] = 0.2
 	LOOT["Hydrocraft.HCDentures"] = 0.2
 	LOOT["Hydrocraft.HCEyepatch"] = 0.2
@@ -54,7 +101,7 @@ local LOOT = {}
 	LOOT["Hydrocraft.HCGlasseye"] = 0.2
 	LOOT["Hydrocraft.HCProstheticleg"] = 0.2
 
--- Food --
+	-- Food --
 	LOOT["Hydrocraft.HCTrailmix"] = 0.1
 	LOOT["Hydrocraft.HCEnergybar"] = 0.1
 	LOOT["Hydrocraft.HCGum"] = 0.2
@@ -63,56 +110,22 @@ local LOOT = {}
 	LOOT["Hydrocraft.HCMysteryseedspacket"] = 0.1
 	LOOT["Hydrocraft.HCMRE"] = 0.2
 
--- Other --
+	-- Other --
 	LOOT["Hydrocraft.HCHousefly"] = 0.2
 	LOOT["Hydrocraft.HCMaggot"] = 0.2
 	LOOT["Base.Cockroach"] = 0.25
 	LOOT["Base.Worm"] = 0.25
 	LOOT["Base.Book"] = 0.2
-
 	LOOT["Base.WaterBottleEmpty"] = 0.3
 	LOOT["Base.BandageDirty"] = 0.2
 	LOOT["Base.Headphones"] = 0.2
 
-
-local function calculate() 
-	
-	local tmp = 0
+	local total = 0
 	for k, v in pairs(LOOT) do
-		tmp = tmp + LOOT[k]
-	end
-	return tmp
-	
-end
-
-local TOTAL = calculate()
-
-local function pickRandomItem()
-
-	local r = ZombRandFloat(0.0, TOTAL)
-	local tmp = 0
-	for k, v in pairs(LOOT) do
-		tmp = tmp + LOOT[k]
-		if(r < tmp) then
-			return InventoryItemFactory.CreateItem(k)
-		end
-	end	
-	return nil
-end
-
-local function SpawnLootOnDeadZombie(zombie)
-	
-	local randomRolls = ROLLS[ZombRand(#(ROLLS)) + 1]
-	
-	for i=1, randomRolls, 1 do
-		local item = pickRandomItem()
-		if(item == nil) then
-			print("Exception / Error - SpawnOnZeds.lua, pickRandomItem() returned nil")
-		else
-			zombie:getInventory():addItem(item)
-		end
+		total = total + LOOT[k]
 	end
 	
+	TOTAL = total
+	
 end
-
-Events.OnZombieDead.Add(SpawnLootOnDeadZombie)
+Events.OnLoad.Add(OnLoadSpawnOnZeds)
