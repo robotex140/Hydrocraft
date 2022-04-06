0558e2e832c0f0b29c260321f7a4cdf320af68ce
HugoQwerty <HugoQwertyZomboid@protonmail.com>
2022-04-06 11:03:17 +0100

    Carpentry bench info popup
    Added an info popup when player builds a Carpenter's Bench, hopefully this will help to cut down the number of confused players.

---
+++ Contents/mods/Hydrocraft/media/lua/client/DisplayInfo.lua
@@ -0,0 +1,4 @@
+
+function DisplayInfoCarpentersBench(items, result, player)
+luautils.okModal("IMPORTANT:\nBefore you can use the Carpenter's Bench you need to plaace it.\nThe steps to do this are:\n1) Right-click in the world\n2) Open the 'Building' menu, then click on 'Carpenter's Workbench'\n3) Select where you want to place the bench\n(A hammer is also required to place the bench)\nNote: Some recipes require that you stand next to the placed bench", true)
+end
