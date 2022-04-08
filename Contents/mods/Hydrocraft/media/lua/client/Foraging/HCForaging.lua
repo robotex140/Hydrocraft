98da0b05ca3443b247dc1495d4c5057da479b9fa
HugoQwerty <HugoQwertyZomboid@protonmail.com>
2022-04-08 16:44:38 +0100

    Update HCForaging.lua
    decided to remove the fatigue penalty to foraging, the time it takes feels like enough of a price to pay.

--- Contents/mods/Hydrocraft/media/lua/client/Foraging/HCForaging.lua
+++ Contents/mods/Hydrocraft/media/lua/client/Foraging/HCForaging.lua
@@ -798,7 +798,7 @@ local function EveryOneMinute()
 
 	if(xp == true) then
 		_player:getXp():AddXP(Perks.PlantScavenging, 2)
-		_player:getStats():setFatigue(_player:getStats():getFatigue()+0.05)
+		--_player:getStats():setFatigue(_player:getStats():getFatigue()+0.05)
 	end
 	
 end
