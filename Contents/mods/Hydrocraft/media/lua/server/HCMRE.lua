e45418fdad913b0c65c4fa7ae52939c7c2d9c352
HugoQwerty <HugoQwertyZomboid@protonmail.com>
2022-04-09 13:08:32 +0100

    Merge branch 'main' of https://github.com/robotex140/Hydrocraft
    Small changes, removed needless ZombRand calls, and repeated calls to getInventory()

--- Contents/mods/Hydrocraft/media/lua/server/HCMRE.lua
+++ Contents/mods/Hydrocraft/media/lua/server/HCMRE.lua
@@ -1,31 +1,33 @@
+--HCMRE.lua
+
 --Open MRE Package
 function recipe_HCMRE_OpenPackage(items, result, player)
-	player:getInventory():AddItem("Hydrocraft.HCMREmeal");
-	player:getInventory():AddItem("Hydrocraft.HCMREmealside");
-	player:getInventory():AddItem("Hydrocraft.HCMREFRH");
-	player:getInventory():AddItem("Hydrocraft.HCMREcrackers");
-	player:getInventory():AddItem("Hydrocraft.HCMREbread");
-	player:getInventory():AddItem("Hydrocraft.HCPurifyingtablets");
-    player:getInventory():AddItem("Hydrocraft.HCPurifyingtablets");
-	player:getInventory():AddItem("Hydrocraft.AlcoholWipes");
-    local getsA = ZombRand(1);
-    local itemSetA = {"Hydrocraft.HCCandymnm", "Hydrocraft.HCCandyrainbow", "Hydrocraft.HCCandycorn", "Hydrocraft.HCGum", "Hydrocraft.HCGum2", "Hydrocraft.HCGum3", "Base.Chocolate", "Hydrocraft.HCGummyworms", "Hydrocraft.HCChocolatedark", "Hydrocraft.HCGummybears", "Hydrocraft.HCJellybeans", "Hydrocraft.HCTaffy", "Hydrocraft.HCEnergybar"};
-    for x=0, getsA do
-        HCItemGet(player, itemSetA);
-  end
+	local inv = player:getInventory()
+	inv:AddItem("Hydrocraft.HCMREmeal");
+	inv:AddItem("Hydrocraft.HCMREmealside");
+	inv:AddItem("Hydrocraft.HCMREFRH");
+	inv:AddItem("Hydrocraft.HCMREcrackers");
+	inv:AddItem("Hydrocraft.HCMREbread");
+	inv:AddItem("Hydrocraft.HCPurifyingtablets");
+	inv:AddItem("Hydrocraft.HCPurifyingtablets");
+	inv:AddItem("Hydrocraft.AlcoholWipes");
+
+	local items = {"Hydrocraft.HCCandymnm", "Hydrocraft.HCCandyrainbow", "Hydrocraft.HCCandycorn", "Hydrocraft.HCGum", "Hydrocraft.HCGum2", "Hydrocraft.HCGum3", "Base.Chocolate", "Hydrocraft.HCGummyworms", "Hydrocraft.HCChocolatedark", "Hydrocraft.HCGummybears", "Hydrocraft.HCJellybeans", "Hydrocraft.HCTaffy", "Hydrocraft.HCEnergybar"};
+
+	inv:AddItem(items[ZombRand(#items)+1])
+
 end
 
 --Heat MRE With FRH
 function recipe_HCMRE_HeatPackage(items, result, player)
-    local getsA = ZombRand(1);
-    local itemSetA = {"Hydrocraft.HCMREmealprepedA", "Hydrocraft.HCMREmealprepedB", "Hydrocraft.HCMREmealprepedC", "Hydrocraft.HCMREmealprepedD", "Hydrocraft.HCMREmealprepedE", "Hydrocraft.HCMREmealprepedF", "Hydrocraft.HCMREmealprepedG", "Hydrocraft.HCMREmealprepedH", "Hydrocraft.HCMREmealprepedI", "Hydrocraft.HCMREmealprepedJ", "Hydrocraft.HCMREmealprepedK"};
-    for x=0, getsA do
-        HCItemGet(player, itemSetA);
-end
-    local getsB = ZombRand(1);
-    local itemSetB = {"Hydrocraft.HCMREmealsideprepedA", "Hydrocraft.HCMREmealsideprepedB", "Hydrocraft.HCMREmealsideprepedC", "Hydrocraft.HCMREmealsideprepedD", "Hydrocraft.HCMREmealsideprepedE", "Hydrocraft.HCMREmealsideprepedF", "Hydrocraft.HCMREmealsideprepedG", "Hydrocraft.HCMREmealsideprepedH"};
-    for x=0, getsB do
-        HCItemGet(player, itemSetB);
-end
+	local inv = player:getInventory()
+
+	local itemSetA = {"Hydrocraft.HCMREmealprepedA", "Hydrocraft.HCMREmealprepedB", "Hydrocraft.HCMREmealprepedC", "Hydrocraft.HCMREmealprepedD", "Hydrocraft.HCMREmealprepedE", "Hydrocraft.HCMREmealprepedF", "Hydrocraft.HCMREmealprepedG", "Hydrocraft.HCMREmealprepedH", "Hydrocraft.HCMREmealprepedI", "Hydrocraft.HCMREmealprepedJ", "Hydrocraft.HCMREmealprepedK"};
+
+	local itemSetB = {"Hydrocraft.HCMREmealsideprepedA", "Hydrocraft.HCMREmealsideprepedB", "Hydrocraft.HCMREmealsideprepedC", "Hydrocraft.HCMREmealsideprepedD", "Hydrocraft.HCMREmealsideprepedE", "Hydrocraft.HCMREmealsideprepedF", "Hydrocraft.HCMREmealsideprepedG", "Hydrocraft.HCMREmealsideprepedH"};
+	
+	inv:AddItem(itemSetA[ZombRand(#itemSetA)+1])
+	inv:AddItem(itemSetB[ZombRand(#itemSetB)+1])
+	
 end
 
