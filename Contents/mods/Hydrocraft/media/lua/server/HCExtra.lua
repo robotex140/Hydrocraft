74488e50a5a40c8e72da2df1e805dddc0975ca66
HugoQwerty <HugoQwertyZomboid@protonmail.com>
2022-04-19 16:18:53 +0100

    Update HCExtra.lua
    Bug fix, gift was giving an item that doesn't exist (Base.Whiskey), changed to Base.WhiskeyFull.

--- Contents/mods/Hydrocraft/media/lua/server/HCExtra.lua
+++ Contents/mods/Hydrocraft/media/lua/server/HCExtra.lua
@@ -2352,7 +2352,7 @@ end
 
 --Random Gifts
 function HCRandomGiftGet(items, result, player)
-giftSet = {"Hydrocraft.HCBookanarchist", "Hydrocraft.HCBinoculars", "Hydrocraft.HCCamera",  "Hydrocraft.HCCalculator", "Hydrocraft.HCLaserpointer", "Base.Spiffo", "Hydrocraft.HCToyrobot", "Hydrocraft.HCToydrawing",  "Hydrocraft.HCDVDPlayer", "Base.Book", "Base.Whiskey", "Base.Wine", "Base.Wine2", "Base.FishingRod", "Base.Headphones",  "Base.Radio", "Base.DigitalWatch", "Hydrocraft.HCFlashlightoff","Hydrocraft.HCBookbedtime", "Hydrocraft.HCBookfairytale", "Hydrocraft.HCDogwhistle"};
+giftSet = {"Hydrocraft.HCBookanarchist", "Hydrocraft.HCBinoculars", "Hydrocraft.HCCamera",  "Hydrocraft.HCCalculator", "Hydrocraft.HCLaserpointer", "Base.Spiffo", "Hydrocraft.HCToyrobot", "Hydrocraft.HCToydrawing",  "Hydrocraft.HCDVDPlayer", "Base.Book", "Base.WhiskeyFull", "Base.Wine", "Base.Wine2", "Base.FishingRod", "Base.Headphones",  "Base.Radio", "Base.DigitalWatch", "Hydrocraft.HCFlashlightoff","Hydrocraft.HCBookbedtime", "Hydrocraft.HCBookfairytale", "Hydrocraft.HCDogwhistle"};
     gift = ZombRand(#giftSet) + 1;
     player:getInventory():AddItem(giftSet[gift]);
 end
