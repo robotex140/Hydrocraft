de4ee647bdcb219271413a670c086ea760930167
HugoQwerty <HugoQwertyZomboid@protonmail.com>
2023-03-17 11:15:51 +0000

    Update HCLoot_2_LocationDistributions.lua

--- Contents/mods/Hydrocraft/media/lua/server/Items/HCLoot_2_LocationDistributions.lua
+++ Contents/mods/Hydrocraft/media/lua/server/Items/HCLoot_2_LocationDistributions.lua
@@ -2,11 +2,13 @@
 --- Adds relative distributions to the  different locations
 --- 2021-11-17 by lorgalis
 ------------------------------------------------------------
-require "Items/ProceduralDistributions";
-require "HCDistributionFunctions";
-require "Items/HCLoot_1_RelativeDistributions";
+--require "Items/ProceduralDistributions";
+--require "HCDistributionFunctions";
+--require "Items/HCLoot_1_RelativeDistributions";
 
-createProcDistributionContainer("storageunit","crate",         "HCCrateLootBoxes");
-createProcDistributionContainer("storageunit","metal_shelves", "HCCrateLootShelves");
-insertItemListsInProcDistribution( "HCCrateLootBoxes", {           lootboxes(10),  } );
-insertItemListsInProcDistribution( "HCCrateLootShelves", {         lootboxes(1),   } );
+--createProcDistributionContainer("storageunit","crate",         "HCCrateLootBoxes");
+--createProcDistributionContainer("storageunit","metal_shelves", "HCCrateLootShelves");
+--insertItemListsInProcDistribution( "HCCrateLootBoxes", {           lootboxes(10),  } );
+--insertItemListsInProcDistribution( "HCCrateLootShelves", {         lootboxes(1),   } );
+
+--These were never used, so have commented them out. -Hugo
